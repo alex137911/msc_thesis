a374b279d5afe9eafc5569fe81854e6d643ad975
Alex Chan <alex.chan1@uwaterloo.ca>
2025-01-29 23:32:57 -0500

    Update HGDP processing to remove CARTaGENE sample IDs after intersection

--- process_HGDPgeno.sh
+++ process_HGDPgeno.sh
@@ -4,10 +4,10 @@
 #SBATCH --job-name=process_HGDPgeno
 #SBATCH --output=process_HGDPgeno.out
 #SBATCH --error=process_HGDPgeno.err
-#SBATCH --time=4:00:00
+#SBATCH --time=48:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=10G
 
 # --------------------------------------------------------------
 # Script efficiency (39708679)
@@ -32,24 +32,23 @@ module load bcftools/1.19
 IN_DIR="/lustre06/project/6061810/shared/HGDP_1KG/unfiltered_vcfs"
 CAG_FILTERED_DIR="/lustre07/scratch/chanalex/CARTaGENE/QC"
 OUT_DIR="/lustre07/scratch/chanalex/HGDP-1KG/QC/HGDP-CAG_Subset"
-HGDP_ONLY_DIR="/lustre07/scratch/chanalex/HGDP-1KG/QC/HGDP-CAG_Subset"
 
 mkdir -p "$OUT_DIR"
 
-# Chromosomes to process
-# CHROMOSOMES=($(seq -f "chr%g" 1 22))
-CHROMOSOMES=("chr21")
-
-# Remove CARTaGENE sample IDs (from HGDP-CAG subset when using bcftools isec)
+# List of HGDP sample IDs (need to remove CARTaGENE IDs from HGDP-CAG subset when using bcftools isec)
 HGDP_IDS="/lustre06/project/6050814/chanalex/msc_thesis/Data/HGDP/complete_HGDPsamples.txt"
+
+# Chromosomes to process
+CHROMOSOMES=($(seq -f "chr%g" 1 22))
+# CHROMOSOMES=("chr21")
 
 # Loop through chromosomes
 for CHR in "${CHROMOSOMES[@]}"; do
+
     # Input files
     HGDP_VCF="$IN_DIR/gnomad.genomes.v3.1.2.hgdp_tgp.${CHR}.vcf.bgz"
     CAG_FILTERED_VCF="$CAG_FILTERED_DIR/${CHR}_filtered.vcf.gz"
     OUTPUT_VCF="$OUT_DIR/${CHR}_HGDP_subset.vcf.gz"
-    HGDP_ONLY_VCF="${HGDP_ONLY_DIR}/${CHR}_HGDP_only.vcf.gz"
 
     # Check that input files exist
     if [[ -f "$HGDP_VCF" && -f "$CAG_FILTERED_VCF" ]]; then
@@ -78,23 +77,26 @@ for CHR in "${CHROMOSOMES[@]}"; do
         bcftools index -t "$OUT_DIR/0001.vcf.gz"
 
         # Merge the intersection into a single VCF
-        bcftools merge -Oz -o "$OUTPUT_VCF" "$OUT_DIR/0000.vcf.gz" "$OUT_DIR/0001.vcf.gz"
+        bcftools merge \
+            --force-samples \
+            -Oz \
+            -o "$OUT_DIR/merged_all.vcf.gz" \
+            "$OUT_DIR/0000.vcf.gz" "$OUT_DIR/0001.vcf.gz"
 
-        # Index the output VCF
-        bcftools index -t "$OUTPUT_VCF"
+        bcftools index -t "$OUT_DIR/merged_all.vcf.gz"
 
-        # Subset the VCF to only include HGDP samples
+        # Remove CARTaGENE sample IDs from HGDP subset
         bcftools view \
             -S "$HGDP_IDS" \
             --force-samples \
             -Oz \
-            -o "$HGDP_ONLY_VCF" \
-            "$OUTPUT_VCF"
+            -o "$OUTPUT_VCF" \
+            "$OUT_DIR/merged_all.vcf.gz"
 
-        # Index the new file
-        bcftools index -t "$HGDP_ONLY_VCF"
-        
+        bcftools index -t "$OUTPUT_VCF"
+
         # Remove intermediate files
+        rm -f "$OUT_DIR/merged_all.vcf.gz" "$OUT_DIR/merged_all.vcf.gz.tbi"
         rm -f "$OUT_DIR/0000.vcf.gz" "$OUT_DIR/0001.vcf.gz" "$OUT_DIR/0000.vcf.gz.tbi" "$OUT_DIR/0001.vcf.gz.tbi"
 
         echo "Subset for $CHR completed: $OUTPUT_VCF"
@@ -104,7 +106,7 @@ for CHR in "${CHROMOSOMES[@]}"; do
     fi
 done
 
-echo "Processing complete."
+echo "Intersection + HGDP sample subset complete."
 
 # --------------------------------------------------------------
 # Load necessary modules
@@ -117,10 +119,9 @@ module load plink/1.9b_6.21-x86_64
 IN_DIR="/lustre07/scratch/chanalex/HGDP-1KG/QC/HGDP-CAG_Subset"
 OUT_DIR="/lustre07/scratch/chanalex/HGDP-1KG/QC"
 
-# Remove samples which fail gnomAD QC hard filters
-# Remove samples which were contaminated
+# Only keep samples which passed gnomAD QC hard filters and were not contaminated
 # Two samples per line for PLINK
-REMOVE_FILE="${IN_DIR}/HGDP_1KG.PassedQC-PLINK.txt"
+PASSED_SAMPLES="${IN_DIR}/HGDP_1KG.PassedQC-PLINK.txt"
 
 # Set thresholds
 MAX_SAMPLE_MISSINGNESS=0.05  # 5% genotype missingness per sample (remove samples with call rate < 95%)
@@ -130,15 +131,15 @@ HWE_PVAL=1e-6                # Variants which depart Hardy-Weinberg equilibrium 
 
 # Loop through each chromosome
 for CHR in "${CHROMOSOMES[@]}"; do
+
   INPUT_FILE="$IN_DIR/${CHR}_HGDP_subset.vcf.gz"
-  OUTPUT_FILE="$OUT_DIR/${CHR}_HGDP_filtered.vcf.gz"
   LOG_FILE="$OUT_DIR/${CHR}_HGDP_processing.log"
 
   echo "Processing $INPUT_FILE" > "$LOG_FILE"
 
   # Step 1: Convert VCF to PLINK binary format
-  plink --vcf "$HGDP_ONLY_VCF" \
-        --remove "$REMOVE_FILE" \
+  plink --vcf "$INPUT_FILE" \
+        --keep "$PASSED_SAMPLES" \
         --const-fid \
         --make-bed \
         --out "${OUT_DIR}/${CHR}_temp" \
@@ -161,13 +162,10 @@ for CHR in "${CHROMOSOMES[@]}"; do
 
   # Consolidate PLINK logs into one file
   cat "${OUT_DIR}/${CHR}_temp.log" "${OUT_DIR}/${CHR}_HGDP_filtered.log" >> "$LOG_FILE"
-  rm -f "${OUT_DIR}/${CHR}_temp.log" "${OUT_DIR}/${CHR}_HGDP_filtered.log" \
-        "${OUT_DIR}/${CHR}_temp".*
+  rm -f "${OUT_DIR}/${CHR}_temp.log" "${OUT_DIR}/${CHR}_HGDP_filtered.log"
 
+  # Cleanup intermediate PLINK files
+  rm -f "${OUT_DIR}/${CHR}_temp".*
+  
   echo "Finished processing $INPUT_FILE" >> "$LOG_FILE"
-
-  # Cleanup intermediate files
-  rm -f "${OUT_DIR}/${CHR}_temp".* \
-      "$HGDP_ONLY_VCF" \
-      "${HGDP_ONLY_VCF}.tbi"
 done
